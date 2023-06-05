e012c49c677db6195b3a85138b2f513e771a32cf
Lyjia <me@lyjia.us>
2023-06-05 16:13:01 -0700

    add LaplacianBlurDetector.py

---
+++ windows/detectors/LaplacianBlurDetector.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+import numpy as np
+
+
+class LapLacianBlurDetector():
+
+    def __init__(self, image_path, threshold):
+        self.threshold = threshold
+        self.image_path = image_path
+        self.image = None
+        self.gray = None
+        self.variance = None
+        self.analyze()
+
+    def analyze(self):
+        self.image = self.get_image()
+        self.gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        self.variance = self.get_laplacian_variance()
+
+    def get_image(self):
+        return cv2.imread(self.image_path)
+
+    def get_laplacian_variance(self):
+        self.variance = cv2.Laplacian(self.gray, cv2.CV_64F).var()
+        return self.variance
+
+    def is_blurry(self, image_path):
+        return self.get_laplacian_variance(self.gray) < self.threshold
