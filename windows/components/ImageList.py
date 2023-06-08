52585ca7cf50ee251dfe14633bd4d0b667d7c9d2
Lyjia <me@lyjia.us>
2023-06-07 17:57:51 -0700

    switch selection mode to checkboxes

--- windows/components/ImageList.py
+++ windows/components/ImageList.py
@@ -14,7 +14,7 @@ class ImageList(QListView):
         self.setIconSize(QSize(192,192))
         self.setMovement(QListView.Movement.Static)
         self.setResizeMode(QListView.ResizeMode.Adjust)
-        self.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
+        #self.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
 
     # you will need to call update_viewed_filter to display selected filter
     # immediately after calling this
